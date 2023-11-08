e80f27a136f004c4fa7443bc005a76948105ae84
arjunloomba1311 <arjunrajloomba@gmail.com>
2023-11-07 18:27:08 -0800

    base changes to design templates

--- app/page.tsx
+++ app/page.tsx
@@ -26,35 +26,23 @@ export default function Home() {
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<Particles
 				className="absolute inset-0 -z-10 animate-fade-in"
-				quantity={100}
+				quantity={200}
 			/>
 			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				chronark
+				Arjun
 			</h1>
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 mx-6">
-					Hi, my name is Andreas, I'm building serverless and open source
-					solutions at{" "}
-					<Link
-						target="_blank"
-						href="https://upstash.com"
-						className="underline duration-500 hover:text-zinc-300"
-					>
-						Upstash
-					</Link>
-
-					
-					<wbr /> and working on{" "}
-					<Link
+					Hi, my name is Arjun, I'm a Junior studying Computer Science at UCLA
+					{/* <Link
 						target="_blank"
 						href="https://unkey.dev"
 						className="underline duration-500 hover:text-zinc-300"
 					>
 						unkey.dev
-					</Link>{" "}
-					at night.
+					</Link>{" "} */}
 				</h2>
 			</div>
 		</div>
